525ee13de650a771228c04c5ce35b5dfd15873be
ry1015 <ry1015@gmail.com>
2014-11-13 14:51:52 -0800

    Made some changes.

--- f8l_exception/includes/inc_text_menu.php
+++ f8l_exception/includes/inc_text_menu.php
@@ -1,6 +1,7 @@
 <br />
 <a href="http://www.joe-james.net/f8l_exception/index.php">Home</a> |
-<a href="http://www.joe-james.net/f8l_exception/new_customer.php">New Customer</a>
+<a href="http://www.joe-james.net/f8l_exception/new_customer.php">New Customer</a> |
+<a href="http://www.joe-james.net/f8l_exception/login.php">Login</a>
 <br />  
 
 <a href="http://www.joe-james.net/f8l_exception/admin.php">Admin</a>

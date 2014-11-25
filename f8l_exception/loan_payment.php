1580710b531ef896dfce84b25219396f041b9071
joeyajames <joeyajames@yahoo.com>
2014-11-24 16:32:19 -0800

    New Loan and Loan Payment both working
    I wrote/debugged these on my online server, so they may need to be
    adapted to run on localHost, and some of the variable names in the
    queries may not match up.

--- f8l_exception/loan_payment.php
+++ f8l_exception/loan_payment.php
@@ -1,25 +1,25 @@
-<!-- F8L Exception Online Bank | Make a Loan Payment -->
+<?php
+session_start(); ?>
+<!-- F8L Exception Online Bank | Loan Payment -->
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>F8L Exception Online Bank | Make a Loan Payment</title>
+	<title>F8L Exception Online Bank | Loan Payment</title>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 	<?php include 'includes/inc_header.php'; ?>
-	
+	<h1>Loan Payment</h1><hr />
 </head>
 <body>
-        <hr />
-        <h1>Make a Loan Payment -- Under construction</h1>
-<?php
-include 'includes/inc_validatePassword.php';
-include 'includes/inc_validateEmail.php';
-include 'includes/inc_validateInput.php';
-include 'includes/inc_validateUserName.php';
 
-function createNewAccount($First,$Last,$Email,$Login,$Password) {
+<?php
+include 'includes/inc_validateInput.php';
+
+function makeLoanPayment($userName, $loanId, $amount) {
 	global $errorCount;
+	global $errorMessage;
+	$newBalance = 0;
 	include 'includes/inc_dbConnect.php';
 	
 	// Select database.
@@ -31,90 +31,80 @@ function createNewAccount($First,$Last,$Email,$Login,$Password) {
 			echo "<p>Connection error. Please try again later.</p>";
 		else {
 			$today = date("Ymd");
-			$TableName = "account";
-			$SQLstring = "INSERT INTO 
-				$TableName (login,password,firstName,lastName,email,active,dateOpened) 
-				VALUES ('$Login','$Password','$First','$Last','$Email',1,'$today')";
-			
+			$dueDate = date('Y-m-d', strtotime("+30 days")); // set due date to 30 days after today
+			$SQLstring = "UPDATE loan 
+				SET balance=balance-'$amount', paymentDueDate='$dueDate', paymentDate='$today'
+				WHERE loanId='$loanId'";
 			$QueryResult = @mysql_query($SQLstring, $db_connect);
+			
+			// get new balance
+			$SQLstring2 = "SELECT balance FROM loan WHERE loanId='$loanId'";
+			$QueryResult2 = @mysql_query($SQLstring2, $db_connect);
+			$row = mysql_fetch_assoc($QueryResult2);
+			$newBalance = $row['balance'];
 		}
 		mysql_close($db_connect);
 	}
-	return ($retval);
+	return $newBalance;
 }
 
-function displayForm($First, $Last, $Email, $Login) {
+function displayForm() {
 	global $errorMessage;
 	echo $errorMessage;
-	?>
-	<form name="register" action="register.php" method="post">
-	<p>First Name: <input type="text" name="First" value="<?php echo $First; ?>" /></p>
-	<p>Last Name: <input type="text" name="Last" value="<?php echo $Last; ?>" /></p>
-	<p>Your E-Mail: <input type="text" name="Email" value="<?php echo $Email; ?>" /></p>
-	<p>User Name: <input type="text" name="Login" value="<?php echo $Login; ?>" /></p>
-	<p>Password: <input type="password" name="Password" value="" /></p>
-	<p>Confirm Password: <input type="password" name="Password2" value="" /></p>
 	
-	<p><input type="submit" name="Submit" value="Register" /></p>
+	?>
+	<form name="loan_payment" action="loan_payment.php" method="post">
+	<p>Loan Id: <input type="text" name="loanId"  /></p>
+	<p>Payment Amount: <input type="text" name="amount"  /></p>	
+	<p><input type="submit" name="Submit" value="Submit" /></p>
 	</form>
 	<br /><br />
 	
 	<?php
+	//include 'includes/inc_text_menu.php';
 }
 
 $showForm = TRUE;
 $errorCount = 0;
 $errorMessage = "";
-$First = "";
-$Last = "";
-$Email = "";
-$Login = "";
-$Password = "";
-$Password2 = "";
+$userName = "";
+$userName = $_SESSION['login'];
 
-if (isset($_POST['Submit'])) {
-	$First = validateInput($_POST['First'],"First Name");
-	$Last = validateInput($_POST['Last'],"Last Name");
-	$Email  = validateEmail($_POST['Email'],"E-mail");
-	$Login  = validateUserName($_POST['Login'],"User Name");
-	$Password  = validatePassword($_POST['Password'],$_POST['Password2'],"Password");
-	if($Login == $Password) {
-		$errorMessage .= "Password cannot be the same as user name<br />";
-		$errorCount++;
+// if not logged in, redirect to login page
+if ($userName == "") {
+	echo "You must be logged in to make a loan payment.<br /><br />";
+	$showForm = FALSE;
+}
+else {	
+	echo "User Name: ".$userName."<br />";
+	
+	if (isset($_POST['Submit'])) {
+		$loanId  = validateInput($_POST['loanId'],"Loan Id");
+		$amount  = validateInput($_POST['amount'],"Payment Amount");
+
+		if($amount < 0) {
+			$errorMessage .= "Loan payment must be a positive number.<br />";
+			$errorCount++;
+		}
+		if ($errorCount == 0)
+			$showForm = FALSE;
+		else
+			$showForm = TRUE;
 	}
-	if ($errorCount == 0)
-		$showForm = FALSE;
-	else
-		$showForm = TRUE;
-}
 
-if ($showForm == TRUE) {
-	if ($errorCount > 0) // if there were errors
-		$errorMessage .= "<p>Please re-enter the form information below.</p>\n";
-	displayForm ($First, $Last, $Email, $Login);
+	if ($showForm == TRUE) {
+		if ($errorCount > 0) // if there were errors
+			$errorMessage .= "<p>Please re-enter the form information below.</p>\n";
+		displayForm ();
+	}
+	else {
+		// make payment in db	
+		$newBalance = makeLoanPayment($userName, $loanId, $amount);
+		echo "<p>Loan payment for ".$amount." has been received for Loan Id ".$loanId."</p>";
+		echo "<p>New balance is ".$newBalance."<br /><br />\n";
+	}
 }
-else {
-	// encrypt password
-	//$options = array('cost' => 11);
-	//$password = password_hash($password, PASSWORD_BCRYPT, $options);
-	
-	// create account in db
-	createNewAccount($First,$Last,$Email,$Login,$Password);
-
-	// send confirmation email
-	$SenderAddress = "$First <$Email>";
-	$Headers = "From: $SenderAddress\nCC:$SenderAddress\n";
-	
-	$from = "PVault"; // sender
-    $subject = "PVault Registration Confirmation";
-    $message = $First . ",\nYou have successfully registered for PVault. Now you can Store your documents in the cloud, securely locked inside your own Personal Vault.\n\nThe PVault Team";
-    // message lines should not exceed 70 characters (PHP rule), so wrap it
-    $message = wordwrap($message, 70);
-    // send mail
-    mail($Email,$subject,$message,"From: $from\n");
-	
-    echo "<p>" . $First . "\nyour account has been created. Welcome to PVault!.</p><br /><br />\n";
-}	
+include 'includes/inc_text_menu.php';
 ?>
 
 </body>
--- f8l_exception/new_loan.php
+++ f8l_exception/new_loan.php
@@ -1,3 +1,5 @@
+<?php
+session_start(); ?>
 <!-- F8L Exception Online Bank | New Loan -->
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
@@ -7,19 +9,16 @@
 	<title>F8L Exception Online Bank | New Loan</title>
 	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 	<?php include 'includes/inc_header.php'; ?>
-	
+	<h1>New Loan</h1><hr />
 </head>
 <body>
-        <hr />
-        <h1>New Loan -- Under construction</h1>
-<?php
-include 'includes/inc_validatePassword.php';
-include 'includes/inc_validateEmail.php';
-include 'includes/inc_validateInput.php';
-include 'includes/inc_validateUserName.php';
 
-function createNewAccount($First,$Last,$Email,$Login,$Password) {
+<?php
+include 'includes/inc_validateInput.php';
+
+function openNewLoan($userName,$balance) {
 	global $errorCount;
+	global $errorMessage;
 	include 'includes/inc_dbConnect.php';
 	
 	// Select database.
@@ -31,90 +30,77 @@ function createNewAccount($First,$Last,$Email,$Login,$Password) {
 			echo "<p>Connection error. Please try again later.</p>";
 		else {
 			$today = date("Ymd");
-			$TableName = "account";
+			$dueDate = date('Y-m-d', strtotime("+30 days")); // set due date to 30 days after today
 			$SQLstring = "INSERT INTO 
-				$TableName (login,password,firstName,lastName,email,active,dateOpened) 
-				VALUES ('$Login','$Password','$First','$Last','$Email',1,'$today')";
-			
+				loan (username, amount, balance, dateOpened, paymentDueDate) 
+				VALUES ('$userName', '$balance', '$balance', '$today', '$dueDate')";
 			$QueryResult = @mysql_query($SQLstring, $db_connect);
+			
+			// get loan id
+			$SQLstring2 = "SELECT max(loanid) as loanId FROM loan;";
+			$QueryResult2 = @mysql_query($SQLstring2, $db_connect);
+			$row = mysql_fetch_assoc($QueryResult2);
+			$loanId = $row['loanId'];
 		}
 		mysql_close($db_connect);
 	}
-	return ($retval);
+	return $loanId;
 }
 
-function displayForm($First, $Last, $Email, $Login) {
+function displayForm() {
 	global $errorMessage;
 	echo $errorMessage;
-	?>
-	<form name="register" action="register.php" method="post">
-	<p>First Name: <input type="text" name="First" value="<?php echo $First; ?>" /></p>
-	<p>Last Name: <input type="text" name="Last" value="<?php echo $Last; ?>" /></p>
-	<p>Your E-Mail: <input type="text" name="Email" value="<?php echo $Email; ?>" /></p>
-	<p>User Name: <input type="text" name="Login" value="<?php echo $Login; ?>" /></p>
-	<p>Password: <input type="password" name="Password" value="" /></p>
-	<p>Confirm Password: <input type="password" name="Password2" value="" /></p>
 	
-	<p><input type="submit" name="Submit" value="Register" /></p>
+	?>
+	<form name="new_loan" action="new_loan.php" method="post">
+	<p>Loan Amount: <input type="text" name="balance"  /></p>	
+	<p><input type="submit" name="Submit" value="Submit" /></p>
 	</form>
 	<br /><br />
 	
 	<?php
+	//include 'includes/inc_text_menu.php';
 }
 
 $showForm = TRUE;
 $errorCount = 0;
 $errorMessage = "";
-$First = "";
-$Last = "";
-$Email = "";
-$Login = "";
-$Password = "";
-$Password2 = "";
+$userName = "";
+$userName = $_SESSION['login'];
 
-if (isset($_POST['Submit'])) {
-	$First = validateInput($_POST['First'],"First Name");
-	$Last = validateInput($_POST['Last'],"Last Name");
-	$Email  = validateEmail($_POST['Email'],"E-mail");
-	$Login  = validateUserName($_POST['Login'],"User Name");
-	$Password  = validatePassword($_POST['Password'],$_POST['Password2'],"Password");
-	if($Login == $Password) {
-		$errorMessage .= "Password cannot be the same as user name<br />";
-		$errorCount++;
+// if not logged in, redirect to login page
+if ($userName == "") {
+	echo "You must be logged in to open a new loan.<br /><br />";
+	$showForm = FALSE;
+}
+else {	
+	echo "User Name: ".$userName."<br />";
+	
+	if (isset($_POST['Submit'])) {
+		$balance  = validateInput($_POST['balance'],"Loan Amount");
+
+		if($balance < 0) {
+			$errorMessage .= "Loan amount must be a positive number.<br />";
+			$errorCount++;
+		}
+		if ($errorCount == 0)
+			$showForm = FALSE;
+		else
+			$showForm = TRUE;
 	}
-	if ($errorCount == 0)
-		$showForm = FALSE;
-	else
-		$showForm = TRUE;
-}
 
-if ($showForm == TRUE) {
-	if ($errorCount > 0) // if there were errors
-		$errorMessage .= "<p>Please re-enter the form information below.</p>\n";
-	displayForm ($First, $Last, $Email, $Login);
+	if ($showForm == TRUE) {
+		if ($errorCount > 0) // if there were errors
+			$errorMessage .= "<p>Please re-enter the form information below.</p>\n";
+		displayForm ();
+	}
+	else {
+		// create loan in db	
+		$loanId = openNewLoan($userName,$balance);
+		echo "<p>New loan has been created for ".$userName." with Loan Id ".$loanId." for ".$balance."</p><br /><br />\n";
+	}
 }
-else {
-	// encrypt password
-	//$options = array('cost' => 11);
-	//$password = password_hash($password, PASSWORD_BCRYPT, $options);
-	
-	// create account in db
-	createNewAccount($First,$Last,$Email,$Login,$Password);
-
-	// send confirmation email
-	$SenderAddress = "$First <$Email>";
-	$Headers = "From: $SenderAddress\nCC:$SenderAddress\n";
-	
-	$from = "PVault"; // sender
-    $subject = "PVault Registration Confirmation";
-    $message = $First . ",\nYou have successfully registered for PVault. Now you can Store your documents in the cloud, securely locked inside your own Personal Vault.\n\nThe PVault Team";
-    // message lines should not exceed 70 characters (PHP rule), so wrap it
-    $message = wordwrap($message, 70);
-    // send mail
-    mail($Email,$subject,$message,"From: $from\n");
-	
-    echo "<p>" . $First . "\nyour account has been created. Welcome to PVault!.</p><br /><br />\n";
-}	
+include 'includes/inc_text_menu.php';
 ?>
 
 </body>
